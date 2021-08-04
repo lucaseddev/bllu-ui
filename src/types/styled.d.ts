0ed0bfdbd0986bfed7d98af2ce12029974e1169b
Lucas Duarte <57420426+lucaseddev@users.noreply.github.com>
2021-08-04 18:55:13 -0300

    Overwritten styled components theme interface.

---
+++ src/types/styled.d.ts
@@ -0,0 +1,27 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    colors: {
+      primary: string;
+      onPrimary: string;
+
+      secondary: string;
+      onSecondary: string;
+
+      background: string;
+      onBackground: string;
+
+      onSurface: string;
+      surface: string;
+
+      error: string;
+      onError: string;
+      underError: string;
+
+      onSuccess: string;
+      success: string;
+      underSuccess: string;
+    };
+  }
+}
