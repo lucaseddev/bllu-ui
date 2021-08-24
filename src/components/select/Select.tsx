a6d11a417038cf0648639ebcbb43f45395775286
Lucas Duarte <lucaseddev@gmail.com>
2021-08-24 20:04:02 -0300

    Styling options list.

--- src/components/select/Select.tsx
+++ src/components/select/Select.tsx
@@ -9,6 +9,8 @@ export interface SelectProps {
   width?: string | number;
 
   size?: SMALL | MEDIUM | LARGE;
+
+  options: never[];
 }
 
 const sizes = {
@@ -17,7 +19,17 @@ const sizes = {
   lg: { height: pxStep(6), fontSize: remStep(7, StepSize.REM125) },
 };
 
-const selectStyle: StyleFunction<SelectProps> = ({
+type StyleSelectProps = Omit<SelectProps, "options">;
+
+const WrapperStyle: StyleFunction<StyleSelectProps> = ({
+  theme,
+  width,
+}) => ({
+  position: "relative",
+  width: width || "fit-content",
+});
+
+const SelectStyle: StyleFunction<StyleSelectProps> = ({
   theme,
   width,
   size,
@@ -25,7 +37,6 @@ const selectStyle: StyleFunction<SelectProps> = ({
   ...sizes[size || MEDIUM],
   display: "flex",
   alignItems: "center",
-  width: width || "fit-content",
 
   paddingLeft: pxStep(3, StepSize.PX4),
   paddingRight: pxStep(3, StepSize.PX4),
@@ -53,33 +64,81 @@ const selectStyle: StyleFunction<SelectProps> = ({
   },
 });
 
-export function Select(props: SelectProps) {
-  const { size = "md" } = props;
+const ListStyle: StyleFunction<StyleSelectProps> = ({ theme }) => ({
+  maxHeight: "180px",
+  overflowY: "auto",
+  margin: 0,
+  borderTop: 0,
+  background: "white",
+  position: "absolute",
+  zIndex: 1000,
+  listStyle: "none",
+  padding: `${pxStep(1, StepSize.PX4)} 0`,
+  minWidth: "100%",
 
-  const style = useStyles([selectStyle], { size });
-  // const {
-  //   isOpen,
-  //   selectedItem,
-  //   getToggleButtonProps,
-  //   getLabelProps,
-  //   getMenuProps,
-  //   highlightedIndex,
-  //   getItemProps,
-  //   inputValue,
-  //   closeMenu,
-  //   openMenu,
-  //   reset,
-  //   selectItem,
-  //   setHighlightedIndex,
-  //   toggleMenu
-  // } = useSelect({ items: [] });
+  marginTop: pxStep(3, StepSize.PX2),
+
+  boxShadow: `
+  0 3.4px 2.7px rgba(0, 0, 0, 0.012),
+  0 8.7px 6.9px rgba(0, 0, 0, 0.018),
+  0 17.7px 14.2px rgba(0, 0, 0, 0.022),
+  0 36.5px 29.2px rgba(0, 0, 0, 0.028),
+  0 100px 80px rgba(0, 0, 0, 0.04)
+  `,
+
+  "& li": {
+    padding: `${pxStep(3, StepSize.PX4)} ${pxStep(6, StepSize.PX4)}`,
+    fontSize: remStep(7, StepSize.REM125),
+  },
+});
+
+export function Select(props: SelectProps) {
+  const { size = "md", options = [], width } = props;
+
+  const wrapperStyle = useStyles([WrapperStyle], { width });
+  const selectStyle = useStyles([SelectStyle], { size });
+  const listStyle = useStyles([ListStyle]);
+
+  const {
+    isOpen,
+    selectedItem,
+    getToggleButtonProps,
+    getLabelProps,
+    getMenuProps,
+    highlightedIndex,
+    getItemProps,
+    inputValue,
+    closeMenu,
+    openMenu,
+    reset,
+    selectItem,
+    setHighlightedIndex,
+    toggleMenu,
+  } = useSelect({ items: [] });
 
   return (
-    <div className={style}>
-      <span>Select a value...</span>
-      <span>
-        <BiChevronDown />
-      </span>
+    <div className={wrapperStyle}>
+      <div
+        className={selectStyle}
+        role="button"
+        {...getToggleButtonProps()}
+      >
+        <span>Select a value...</span>
+        <span>
+          <BiChevronDown />
+        </span>
+      </div>
+      <ul {...getMenuProps()} className={listStyle}>
+        {isOpen &&
+          ["Select a value...", ...options].map((item, index) => (
+            <li
+              key={`${item}${index}`}
+              {...getItemProps({ item, index })}
+            >
+              {item}
+            </li>
+          ))}
+      </ul>
     </div>
   );
 }
