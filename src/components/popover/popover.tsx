5eefbd00dd3c6c6a064cc56d604b76ac4269045b
lucaseddev <lucaseddev@gmail.com>
2021-10-05 20:41:19 -0300

    Created basic structure and styling for popover component.

---
+++ src/components/popover/popover.tsx
@@ -0,0 +1,145 @@
+import { VirtualElement } from "@popperjs/core";
+import { Position } from "../../constants";
+import React, { useRef, useState } from "react";
+import { usePopper } from "react-popper";
+import { styled } from "hooks";
+import { pxStep, StepSize } from "helpers/scale";
+import { Portal } from "components/portal";
+
+import { isElement } from "react-is";
+
+export interface PopoverProps {
+  children: React.ReactElement;
+
+  trigger?: "click" | "hover";
+
+  placement?: Position;
+
+  content: React.ReactNode;
+}
+
+const PopoverStyle = styled(({ theme }) => ({
+  backgroundColor: theme.colors.default,
+  border: `1px solid ${theme.colors.surfaceStroke}`,
+  borderRadius: pxStep(1, StepSize.PX4),
+
+  padding: `${pxStep(1, StepSize.PX4)} 0px`,
+
+  zIndex: 999,
+
+  boxShadow: `  0px 0px 16px rgba(0, 0, 0, 0.08)`,
+
+  "& > div[data-arrow]": {
+    "&, & > span": {
+      position: "absolute",
+      width: pxStep(2, StepSize.PX4),
+      height: pxStep(2, StepSize.PX4),
+    },
+
+    "& > span": {
+      visibility: "visible",
+    },
+
+    visibility: "hidden",
+  },
+
+  "&[data-popper-placement^='top'] > div[data-arrow]": {
+    bottom: `-${pxStep(2, StepSize.PX4)}`,
+    "& > span": {
+      borderTop: `${pxStep(2, StepSize.PX4)} solid ${
+        theme.colors.default
+      }`,
+      borderLeft: `${pxStep(2, StepSize.PX4)} solid transparent`,
+      borderRight: `${pxStep(2, StepSize.PX4)} solid transparent`,
+    },
+  },
+
+  "&[data-popper-placement^='bottom'] > div[data-arrow]": {
+    top: `-${pxStep(2, StepSize.PX4)}`,
+    "& > span": {
+      borderBottom: `${pxStep(2, StepSize.PX4)} solid ${
+        theme.colors.default
+      }`,
+      borderLeft: `${pxStep(2, StepSize.PX4)} solid transparent`,
+      borderRight: `${pxStep(2, StepSize.PX4)} solid transparent`,
+    },
+  },
+
+  "&[data-popper-placement^='left'] > div[data-arrow]": {
+    right: `-${pxStep(2, StepSize.PX4)}`,
+    "& > span": {
+      borderLeft: `${pxStep(2, StepSize.PX4)} solid ${
+        theme.colors.default
+      }`,
+      borderTop: `${pxStep(2, StepSize.PX4)} solid transparent`,
+      borderBottom: `${pxStep(2, StepSize.PX4)} solid transparent`,
+    },
+  },
+
+  "&[data-popper-placement^='right'] > div[data-arrow]": {
+    left: `-${pxStep(2, StepSize.PX4)}`,
+    "& > span": {
+      borderRight: `${pxStep(2, StepSize.PX4)} solid ${
+        theme.colors.default
+      }`,
+      borderTop: `${pxStep(2, StepSize.PX4)} solid transparent`,
+      borderBottom: `${pxStep(2, StepSize.PX4)} solid transparent`,
+    },
+  },
+}));
+
+export function Popover(props: PopoverProps) {
+  const {
+    trigger,
+    children,
+    placement = Position.BOTTOM,
+    content,
+  } = props;
+
+  if (!isElement(children)) {
+    console.warn(
+      "Popover children can only contain react HTML elements!"
+    );
+    throw new Error(
+      "Popover children can only contain react HTML elements!"
+    );
+  }
+
+  const [arrowRef, setArrowRef] = useState<HTMLDivElement | null>(
+    null
+  );
+  const [childRef, setChildRef] = useState(null);
+  const [popperRef, setPopperRef] = useState<HTMLDivElement | null>(
+    null
+  );
+
+  const { styles, attributes } = usePopper(childRef, popperRef, {
+    modifiers: [
+      { name: "arrow", options: { element: arrowRef } },
+      { name: "offset", options: { offset: [0, 8] } },
+    ],
+    placement: placement,
+  });
+
+  const popperStyle = PopoverStyle();
+
+  return (
+    <React.Fragment>
+      {React.cloneElement(children, { ref: setChildRef })}
+
+      <Portal>
+        <div
+          className={`${popperStyle}`}
+          ref={setPopperRef}
+          style={styles.popper}
+          {...attributes.popper}
+        >
+          {content}
+          <div ref={setArrowRef} style={styles.arrow} data-arrow>
+            <span />
+          </div>
+        </div>
+      </Portal>
+    </React.Fragment>
+  );
+}
---
+++ src/components/popover/stories/popover.stories.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Meta, Story } from "@storybook/react";
+
+import { Popover, PopoverProps } from "..";
+import { Flex } from "../../flex";
+import { Box } from "../../box";
+import { IconButton } from "../../buttons/IconButton";
+import { BiDotsVertical } from "react-icons/bi";
+
+export default {
+  title: "Components/Popover",
+  component: Popover,
+} as Meta;
+
+const Template: Story<PopoverProps> = (args) => (
+  <Flex.Container
+    spacing={8}
+    justifyContent="center"
+    alignItems="center"
+    h="100vh"
+  >
+    <Flex.Item>
+      <Popover {...args} />
+    </Flex.Item>
+  </Flex.Container>
+);
+
+export const PopoverClick = Template.bind({});
+PopoverClick.args = {
+  children: <div>Teste</div>,
+  placement: "top-start",
+  content: (
+    <Box py={1} px={3}>
+      Teste
+    </Box>
+  ),
+};
